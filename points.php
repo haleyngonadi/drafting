585c191de1f71163d2e2a4a23d261da5c7d94b7d
cngonadi <misshaleyn@gmail.com>
2017-06-29 12:51:27 -0400

    Points inner!

--- points.php
+++ points.php
@@ -20,7 +20,9 @@ get_header(); ?>
 $args = array(
 	'post_type' => 'points',
 	'posts_per_page'=>-1,
-	'orderby' => 'date'
+	'orderby' => 'date',
+	'post__not_in' => array( 96, 95, 94, 93, 92, 91, 90, 89, 97, 102, 101, 100  )
+
 	
 );
 $the_query = new WP_Query( $args ); ?>
@@ -33,6 +35,14 @@ $the_query = new WP_Query( $args ); ?>
 	<?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
 		<div class="col-sm-4"><div id="points-title"><a href="<?php the_permalink(); ?>" title="Click for more."><?php the_title(); ?></a></div><span title="<?php the_title()?> is worth <?php echo get_post_meta(get_the_id(), '_point_value', true);?> points."> <?php echo get_post_meta(get_the_id(), '_point_value', true);?> Point(s)</span></div>
 	<?php endwhile; ?>
+
+	<div class="col-sm-4"><div id="points-title"><a href="/points/nominated" title="Click for more."></a></div><span title="A nominated houseguest will cost you -5 points."> -5 Point(s)</span></div>
+
+	<div class="col-sm-4"><div id="points-title"><a href="/points/veto-player" title="Click for more."></a></div><span title="A houseguest picked to play in the veto will earn you 2 points."> 2 Point(s)</span></div>
+
+		<div class="col-sm-4"><div id="points-title"><a href="/points/have-not" title="Click for more."></a></div><span title="Havenot will cost you -2 points."> -2 Point(s)</span></div>
+
+
 	<!-- end of the loop -->
 
 	<!-- pagination here -->
