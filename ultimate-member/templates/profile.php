6f6d935379e2f6200046a50ecfc816e48773084e
haleyngonadi <misshaleyn@gmail.com>
2017-06-28 23:37:48 -0400

    Adde more profile things.

--- ultimate-member/templates/profile.php
+++ ultimate-member/templates/profile.php
@@ -7,23 +7,6 @@
 		<?php if ( um_is_on_edit_profile() ) { ?><form method="post" action=""><?php } ?>
 		
 
-			
-			<?php do_action('um_profile_navbar', $args ); ?>
-			
-			<?php
-				
-			$nav = $ultimatemember->profile->active_tab;
-			$subnav = ( get_query_var('subnav') ) ? get_query_var('subnav') : 'default';
-				
-			print "<div class='um-profile-body $nav $nav-$subnav'>";
-				
-				// Custom hook to display tabbed content
-				do_action("um_profile_content_{$nav}", $args);
-				do_action("um_profile_content_{$nav}_{$subnav}", $args);
-				
-			print "</div>";
-				
-			?>
 		
 		<?php if ( um_is_on_edit_profile() ) { ?></form><?php } ?>
 
@@ -92,5 +75,196 @@ $curauth = um_profile_id();
 
 	
 	</div>
+
+
+		<h3 class="point-name"> <?php echo um_user('display_name'); ?>'s Drafts</h3>
+
+
+
+			<?php
+			$types = get_post_types( array( 'public' => true ) );
+			$args = array(
+			  'numberposts' => -1,
+			  'post_type' => $types,
+			  'meta_query' => array (
+				array (
+				  'key' => '_user_liked',
+				  'value' => $curauth->ID,
+				  'compare' => 'LIKE'
+				)
+			  ) );		
+			$sep = '';
+			$like_query = new WP_Query( $args );
+			if ( $like_query->have_posts() ) : ?>
+			<div class="row">
+			<?php while ( $like_query->have_posts() ) : $like_query->the_post(); 
+			 ?>
+
+			 <div class="col-sm-3">
+
+			 <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
+
+			 	<div class="draft-photo" style=" background-image:url('<?php the_post_thumbnail_url();?>')"></div>
+
+			<span class="draft-name"><?php the_title(); ?></span></a>
+
+			</div>
+
+			<?php
+			endwhile; 
+			?>
+			</div>
+			<?php else : ?>
+			<p><?php _e( 'You have not drafted any players yet.'); ?></p>
+			<?php 
+			endif; 
+			wp_reset_postdata(); 
+			?>
+
+
+<div class="the_weeks row">
+
+<?php 
+
+$feature_meta_fields = array(
+    array(
+        'meta_id'=>  1,
+        'title'  => 'Week 1',
+        'callback' => 'week_one',
+    ),
+    array(
+        'meta_id'=>  2,
+        'title'  => 'Week 2',
+        'callback' => 'week_two',
+    ),
+    array(
+        'meta_id'=>  3,
+        'title'  => 'Week 3',
+        'callback' => 'week_three',
+    ),
+    array(
+         'meta_id'=>  4,
+        'title'  => 'Week 4',
+        'callback' => 'week_four',
+    ),
+    array(
+        'meta_id'=>  5,
+        'title'  => 'Week 5',
+        'callback' => 'week_five',
+    ),
+    array(
+         'meta_id'=>  6,
+        'title'  => 'Week 6',
+        'callback' => 'week_six',
+    ),
+        array(
+        'meta_id'=>  7,
+        'title'  => 'Week 7',
+        'callback' => 'week_seven',
+    ),
+    array(
+        'meta_id'=>  8,
+        'title'  => 'Week 8',
+        'callback' => 'week_eight',
+    ),
+    array(
+        'meta_id'=>  9,
+        'title'  => 'Week 9',
+        'callback' => 'week_nine',
+    ),
+    array(
+         'meta_id'=>  10,
+        'title'  => 'Week 10',
+        'callback' => 'week_ten',
+    ),
+    array(
+        'meta_id'=>  11,
+        'title'  => 'Week 11',
+        'callback' => 'week_eleven',
+    ),
+    array(
+         'meta_id'=>  12,
+        'title'  => 'Week 12',
+        'callback' => 'week_twelve',
+    )
+);
+
+foreach ( $feature_meta_fields as $fields ) : ?>
+
+
+
+
+<?php 
+
+$getit = $fields['callback'];
+
+$user_last = get_user_meta( $curauth->ID, $getit, true ); 
+   	 $array = array_map( 'trim', explode( ',', $user_last ) ); 
+
+?>
+
+<?php if ( !empty( $array) ) : 
+
+
+$args = array('post__in' => $array, 'post_type' => 'points' );
+
+$the_query = new WP_Query( $args ); ?>
+
+
+<?php if ( $the_query->have_posts() ) : ?>
+
+		<section class="weeks <?php echo $fields['callback'];?>" data-value="<?php echo $fields['meta_id'];?>">
+
+
+<?php $meta = get_user_meta(1, 'active', true );
+    if ($fields['meta_id'] == $meta) {
+        echo '<div class="week-active" title="Active Week">';
+    } else {
+        echo '<div class="week-not">';
+      }
+	
+?>
+	
+<div class="week-header">
+<span class="circle-span">stats</span>
+<h3 class="cute-circle"><?php echo $fields['title'];?> </h3>
+</div>
+
+	<!-- pagination here -->
+
+	<!-- the loop -->
+	<div class="scored row">
+	<?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+		<div class="col-sm-3">
+
+		<ul class="listed">
+		<li><span class="list-value"><?php echo get_post_meta( get_the_ID(), '_point_value', true ); ?></span></li>
+		<li><span class="list-title"><?php the_title(); ?></span></li>
+		<li><span class="list-who"><?php 
+
+		$data1 = "points_";
+		$data2 = $fields['callback'];
+		$result = $data1 . '' . $data2;
+
+		echo get_post_meta( get_the_ID(), $result, true ); ?></span></li>
+
+		</ul>
+
+		</div>
+	<?php endwhile; ?></div>
+	<!-- end of the loop -->
+</div></section>
+	<!-- pagination here -->
+
+	<?php wp_reset_postdata(); ?>
+
+
+<?php endif; ?>
+
+<?php endif; ?>
+
+<?php endforeach; ?>
+
+</div>
 	
 </div>
