53fb11dc2d99ccbb1efe37dfbeca2675e7b2020e
cngonadi <misshaleyn@gmail.com>
2017-06-29 13:21:11 -0400

    Added extra fields to points page.

--- single-points.php
+++ single-points.php
@@ -14,6 +14,10 @@ get_header(); ?>
 
         <?php if ( is_single('112')) : ?>
        <h3 class="point-name">Have Not</h3>
+       <?php if ( is_single('110')) : ?>
+       <h3 class="point-name">Nominated</h3>
+       <?php if ( is_single('111')) : ?>
+       <h3 class="point-name">VETO Player</h3>
       <?php else:?>
         <h3 class="point-name"><?php the_title()?></h3>
       <?php endif; ?>
@@ -30,27 +34,82 @@ get_header(); ?>
   </tr>
   <tr>
     <td>Week 1</td>
+
+    <?php if ( is_single('112')) : ?>
+
+      <!---Get Have nots -->
+
+          <td><?php $have_one = get_post_meta(97, 'points_week_one', true);
+    $haveaid = $wpdb->get_var( "SELECT ID FROM $wpdb->posts WHERE post_title = '" . $have_one . "'" );
+    echo '<a href="'.get_permalink($haveaid).'">'.$have_one.'</a>';
+    ?>
+    <?php $have_two = get_post_meta(100, 'points_week_one', true);
+    $havid = $wpdb->get_var( "SELECT ID FROM $wpdb->posts WHERE post_title = '" . $have_two . "'" );
+    echo ', <a href="'.get_permalink($havid).'">'.$have_two.'</a>';
+    ?> 
+    <?php $have_three = get_post_meta(101, 'points_week_one', true);
+    $havbid = $wpdb->get_var( "SELECT ID FROM $wpdb->posts WHERE post_title = '" . $have_three . "'" );
+    echo ', <a href="'.get_permalink($hav3id).'">'.$have_three.'</a>';
+    ?>
+    <?php $have_four = get_post_meta(102, 'points_week_one', true);
+    $havedid = $wpdb->get_var( "SELECT ID FROM $wpdb->posts WHERE post_title = '" . $have_four . "'" );
+    echo ', <a href="'.get_permalink($havedid).'">'.$have_four.'</a>';
+    ?></td>
+
+<!---Get Noms -->
+
+        <?php if ( is_single('110')) : ?>
+
+                    <td><?php $nom_one = get_post_meta(89, 'points_week_one', true);
+    $nomaid = $wpdb->get_var( "SELECT ID FROM $wpdb->posts WHERE post_title = '" . $nom_one . "'" );
+    echo '<a href="'.get_permalink($nomaid).'">'.$nom_one.'</a>';
+    ?><?php $nom_two = get_post_meta(90, 'points_week_one', true);
+    $nombid = $wpdb->get_var( "SELECT ID FROM $wpdb->posts WHERE post_title = '" . $nom_two . "'" );
+    echo ', <a href="'.get_permalink($nombid).'">'.$nom_two.'</a>';
+    ?></td>
+
+<!---Get Veto Players -->
+
+        <?php if ( is_single('11')) : ?>
+
+           <td>
+
+    <?php 
+    echo '<a href="'.get_permalink($wpdb->get_var( "SELECT ID FROM $wpdb->posts WHERE post_title = '" . get_post_meta(91, 'points_week_one', true) . "'" );).'">'.get_post_meta(91, 'points_week_one', true).'</a>';
+    ?>
+
+   <?php 
+    echo ', <a href="'.get_permalink($wpdb->get_var( "SELECT ID FROM $wpdb->posts WHERE post_title = '" . get_post_meta(92, 'points_week_one', true) . "'" );).'">'.get_post_meta(92, 'points_week_one', true).'</a>';
+    ?>
+
+     <?php 
+    echo ', <a href="'.get_permalink($wpdb->get_var( "SELECT ID FROM $wpdb->posts WHERE post_title = '" . get_post_meta(93, 'points_week_one', true) . "'" );).'">'.get_post_meta(93, 'points_week_one', true).'</a>';
+    ?>
+
+       <?php 
+    echo '<a href="'.get_permalink($wpdb->get_var( "SELECT ID FROM $wpdb->posts WHERE post_title = '" . get_post_meta(94, 'points_week_one', true) . "'" );).'">'.get_post_meta(94, 'points_week_one', true).'</a>';
+    ?>
+
+   <?php 
+    echo ', <a href="'.get_permalink($wpdb->get_var( "SELECT ID FROM $wpdb->posts WHERE post_title = '" . get_post_meta(95, 'points_week_one', true) . "'" );).'">'.get_post_meta(95, 'points_week_one', true).'</a>';
+    ?>
+
+     <?php 
+    echo ', <a href="'.get_permalink($wpdb->get_var( "SELECT ID FROM $wpdb->posts WHERE post_title = '" . get_post_meta(96, 'points_week_one', true) . "'" );).'">'.get_post_meta(96, 'points_week_one', true).'</a>';
+    ?>
+
+   </td>
+
+
+
+
+  <?php else:?>
     <td><?php $sham = get_post_meta(get_the_id(), 'points_week_one', true);
     $postid = $wpdb->get_var( "SELECT ID FROM $wpdb->posts WHERE post_title = '" . $sham . "'" );
 		echo '<a href="'.get_permalink($postid).'">'.$sham.'</a>';
     ?></td>
-  </tr>
-    <tr>
-    <td>Week 2</td>
-        <td><?php $sham = get_post_meta(get_the_id(), 'points_week_two', true);
-    $postid = $wpdb->get_var( "SELECT ID FROM $wpdb->posts WHERE post_title = '" . $sham . "'" );
-		echo '<a href="'.get_permalink($postid).'">'.$sham.'</a>';
-    ?></td>
 
-    </tr>
-
-    <tr>
-
-    <td>Week 3</td>
-        <td><?php $sham = get_post_meta(get_the_id(), 'points_week_three', true);
-    $postid = $wpdb->get_var( "SELECT ID FROM $wpdb->posts WHERE post_title = '" . $sham . "'" );
-    echo '<a href="'.get_permalink($postid).'">'.$sham.'</a>';
-    ?></td>
+    <?php endif; ?>
   </tr>
 
   </table>
