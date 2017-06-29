9892a4cda20749212dc2d1dac7890f9f4d2b6f89
haleyngonadi <misshaleyn@gmail.com>
2017-06-28 23:26:38 -0400

    Added profile page

---
+++ profile-page.php
@@ -0,0 +1,21 @@
+<?php 
+
+/* Template Name: Profile */
+get_header(); ?>
+
+	<div id="content" class="clearfix">
+	
+		<?php while ( have_posts() ) : the_post(); ?>
+						
+			<div class="pages-header"><h3 class="point-name"><?php the_title(); ?></h3></div>
+
+			<?php the_content(); ?>
+			
+
+		<?php endwhile; ?>
+		
+	</div>
+	<!-- /#content -->
+		
+	
+<?php get_footer(); ?>
