cbdbef9aba4a6499fae262dcb409f86e5613629d
haleyngonadi <misshaleyn@gmail.com>
2017-06-30 20:11:09 -0400

    Replaced font awesome!

--- header.php
+++ header.php
@@ -47,8 +47,7 @@
 <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/bootstrap.min.css">
 <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
 <link href="//fonts.googleapis.com/css?family=Abril+Fatface|Inconsolata:400,700|Source+Sans+Pro:100, 300,400,400i,600,600i,700,700i,900,900i" rel="stylesheet">
-<link rel="stylesheet" type="text/css" media="all" href="<?php echo get_template_directory_uri(); ?>/css/font-awesome.min.css">
-
+<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 
 <?php
