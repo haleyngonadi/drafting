0a9518286dd559ae5d2eb7f28bbff50811aa3c5f
haleyngonadi <misshaleyn@gmail.com>
2017-07-28 21:08:29 -0400

    Revert "Week Five!"
    This reverts commit 6c83de2038cff54381826d534166ef439624ff6b.

--- js/main.js
+++ js/main.js
@@ -13,16 +13,16 @@ if  (activeclass = 1){
 var arr = [];
 
 
-if( $('.week_five').length )         
+if( $('.week_two').length )         
 {
-    $('.week_five .week-not').removeClass('week-not').addClass('week-active');
+    $('.week_two .week-not').removeClass('week-not').addClass('week-active');
     
 }
 
-if( $('.w_five').length )         
+if( $('.w_two').length )         
 {
 
-    $('.w_five .week-not').removeClass('week-not').addClass('week-active');
+    $('.w_two .week-not').removeClass('week-not').addClass('week-active');
 
 
 	var all = [];
@@ -34,12 +34,12 @@ $('.won-value').each(function(index, elem){
  $('.points-all').countTo({from: 0, to: eval(all.join("+"))});
 
 
-    $(".w_five").find('.won-value').each(function(index, elem){
+    $(".w_two").find('.won-value').each(function(index, elem){
     arr.push($(this).text());
 
 });
 }
- else if( $('.week_five').length ){
+ else if( $('.week_two').length ){
 
  	var all = [];
 $('.list-value').each(function(index, elem){
@@ -52,7 +52,7 @@ $('.list-value').each(function(index, elem){
 
 
 
-   $(".week_five").find('.list-value').each(function(index, elem){
+   $(".week_two").find('.list-value').each(function(index, elem){
     arr.push($(this).text());
 
     });
